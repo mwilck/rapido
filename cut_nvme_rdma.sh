15859ebcba7f13a6354db69cc4ddc22e85096588
Martin Wilck <mwilck@suse.com>
2017-07-19 00:32:57 +0200

    cut_nvme_rdma.sh: workaround for dracut's rootok test
    When started without systemd, but with network, dracut doesn't
    start all necessary services unless "$root" and "$rootok"
    are set after parsing the cmdline hooks (in particular, the
    network isn't brought up). Therefore, fake an "ok" root parameter
    here to trick dracut into continuing. It does no harm because
    we don't want to mount root anyway.

--- cut_nvme_rdma.sh
+++ cut_nvme_rdma.sh
@@ -18,14 +18,27 @@ RAPIDO_DIR="$(realpath -e ${0%/*})"
 _rt_require_dracut_args
 _rt_require_lib "libkeyutils.so.1"
 
+# Pretend to dracut that root was set correctly
+# (we don't want to mount root!)
+HOOK=$(mktemp /tmp/nvme_rdma.XXXXXX)
+[ -n "$HOOK" ] || _fatal "failed to create temp file"
+cat >"$HOOK" <<\EOF
+#! /bin/sh
+: ${root:=NONE}
+rootok=1
+EOF
+
 "$DRACUT" --install "tail blockdev ps rmdir resize dd vim grep find df sha256sum \
 		   strace mkfs.xfs killall nvme insmod \
 		   $LIBS_INSTALL_LIST" \
 	--include "$RAPIDO_DIR/nvme_rdma_autorun.sh" "/.profile" \
 	--include "$RAPIDO_DIR/rapido.conf" "/rapido.conf" \
 	--include "$RAPIDO_DIR/vm_autorun.env" "/vm_autorun.env" \
+	--include "$HOOK" "/lib/dracut/hooks/cmdline/99-fake-ok.sh" \
 	--add-drivers "nvme-core nvme-fabrics nvme-rdma nvmet nvmet-rdma \
 		       rdma_rxe zram lzo ib_core ib_uverbs rdma_ucm crc32" \
 	--modules "bash base network ifcfg" \
 	$DRACUT_EXTRA_ARGS \
 	$DRACUT_OUT
+
+rm -f "$HOOK"
