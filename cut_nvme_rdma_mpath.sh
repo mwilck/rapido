91a26a6d5b893e039a2b43714125a7c111555941
Martin Wilck <mwilck@suse.com>
2018-04-17 21:48:59 +0200

    nvme_rdma_mpath: new scenario

---
+++ cut_nvme_rdma_mpath.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+#
+# Copyright (C) SUSE LINUX GmbH 2017, all rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) version 3.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+
+RAPIDO_DIR="$(realpath -e ${0%/*})"
+. "${RAPIDO_DIR}/runtime.vars"
+
+_rt_require_dracut_args
+_rt_require_nvme_cli
+_rt_require_lib "libkeyutils.so.1"
+_rt_require_multipath
+
+# Pretend to dracut that root was set correctly
+# (we don't want to mount root!)
+HOOK=$(mktemp /tmp/nvme_rdma.XXXXXX)
+[ -n "$HOOK" ] || _fatal "failed to create temp file"
+cat >"$HOOK" <<\EOF
+#! /bin/sh
+: ${root:=NONE}
+rootok=1
+EOF
+
+function get_perl_deps() {
+    scandeps.pl -B "$1"  | \
+	while read x rest; do
+	    [[ $x ]] || continue
+	    [[ a"$x" = "a'feature'" ]] && continue
+	    perl -M${x//\'/} -e 'print "\t$INC{$_} " for keys %INC;'
+	done
+}
+RXE_DEPS="ifconfig touch ibv_devinfo $(get_perl_deps $(which rxe_cfg))"
+
+get_perl_sos () {
+    for pm in "$@"; do
+	case $pm in
+	    *linux-thread-multi/*);;
+	    *) continue;;
+	esac
+	bs=${pm#*linux-thread-multi/}
+	dr=${pm%$bs}
+	echo -n "$(ls $dr/auto/${bs%.pm}/*.so 2>/dev/null) "
+    done
+}
+RXE_DEPS="$RXE_DEPS $(get_perl_sos $RXE_DEPS)"
+
+rm -f $DRACUT_OUT
+"$DRACUT" --install "tail blockdev ps rmdir resize dd vim grep find df sha256sum \
+		   strace mkfs.xfs killall insmod dhclient dhclient-script \
+		   rxe_cfg $LIBS_INSTALL_LIST rdma_server rdma_client \
+		   ethtool netstat \
+		   /etc/libibverbs.d/rxe.driver \
+		   /usr/lib64/libibverbs/librxe-rdmav16.so \
+		   $RXE_DEPS
+		   " \
+	--include "$RAPIDO_DIR/nvme_rdma_mpath_autorun.sh" "/.profile" \
+	--include "$RAPIDO_DIR/rapido.conf" "/rapido.conf" \
+	--include "$RAPIDO_DIR/vm_autorun.env" "/vm_autorun.env" \
+	--include "$HOOK" "/lib/dracut/hooks/cmdline/99-fake-ok.sh" \
+	--include "$RAPIDO_DIR/lib/vlan.sh" /usr/lib/rapido/vlan.sh \
+	--include "$RAPIDO_DIR/lib/zram.sh" /usr/lib/rapido/zram.sh \
+	--include "$RAPIDO_DIR/lib/nvme.sh" /usr/lib/rapido/nvme.sh \
+	--add-drivers "nvme-core nvme-fabrics nvme-rdma nvmet nvmet-rdma \
+		       rdma_rxe zram lzo ib_core ib_uverbs rdma_ucm crc32 \
+		       8021q bfq cfq-iosched deadline-iosched kyber-iosched mq-deadline" \
+	--modules "bash base network ifcfg multipath" \
+	$DRACUT_EXTRA_ARGS \
+	$DRACUT_OUT
+
+rm -f "$HOOK"
+_rt_xattr_cmdline_set $DRACUT_OUT "log_buf_len=4M"
+
+_rt_xattr_qemu_args_set \
+    $DRACUT_OUT \
+    "-object filter-dump,id=dump0,netdev=%NETDEV%,file=/tmp/rap%VM%.pcap"
---
+++ nvme_rdma_mpath_autorun.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+#
+# Copyright (C) SUSE LINUX GmbH 2017, all rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) version 3.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+
+if [ ! -f /vm_autorun.env ]; then
+	echo "Error: autorun scripts must be run from within an initramfs VM"
+	exit 1
+fi
+
+. /vm_autorun.env
+. /usr/lib/rapido/vlan.sh
+. /usr/lib/rapido/zram.sh
+. /usr/lib/rapido/nvme.sh
+
+set -x
+
+#### start udevd
+ps -eo args | grep -v grep | grep /usr/lib/systemd/systemd-udevd \
+	|| /usr/lib/systemd/systemd-udevd --daemon
+
+cat /proc/mounts | grep debugfs &> /dev/null
+if [ $? -ne 0 ]; then
+	mount -t debugfs debugfs /sys/kernel/debug/
+fi
+
+modprobe configfs
+cat /proc/mounts | grep configfs &> /dev/null
+if [ $? -ne 0 ]; then
+	mount -t configfs configfs /sys/kernel/config/
+fi
+
+ip link set eth0 mtu ${BR_MTU:-9000}
+#sleep 5 # give the network stack some time
+
+# Module crc32 is ambiguous on some kernels (kernel/lib and kernel/crypto)
+# MUST load the crypto/crc32 module like this, all else fails.
+##insmod /lib/modules/*/kernel/crypto/crc32.ko
+modprobe ib_core
+modprobe ib_uverbs
+modprobe rdma_ucm
+modprobe rdma-rxe
+modprobe nvme-core
+modprobe nvme-fabrics
+modprobe nvme-rdma
+modprobe nvmet-rdma
+modprobe nvmet
+modprobe zram num_devices="0"
+modprobe bfq
+modprobe cfq-iosched
+modprobe deadline-iosched
+modprobe kyber-iosched
+modprobe mq-deadline
+
+for i in $DYN_DEBUG_MODULES; do
+	echo "module $i +pf" > /sys/kernel/debug/dynamic_debug/control
+done
+for i in $DYN_DEBUG_FILES; do
+	echo "file $i +pf" > /sys/kernel/debug/dynamic_debug/control
+done
+# echo 8 >/proc/sys/kernel/printk
+
+# These flood the log with state machine messages
+#for i in rxe_comp.c rxe_resp.c; do
+#	echo "file $i format \"state = %s\" -pf" > /sys/kernel/debug/dynamic_debug/control
+#done
+
+
+nvmet_cfs="/sys/kernel/config/nvmet"
+SUBSYS="rapnv"
+#UUID=28732221-723c-4900-b794-73919f984fc8
+
+mkdir -p /var/lib/dhcp
+
+echo eth0 > /sys/module/rdma_rxe/parameters/add
+cat >/etc/udev/rules.d/60-sched.rules<<EOF
+ACTION!="add", GOTO="ssd_scheduler_end"
+SUBSYSTEM!="block", GOTO="ssd_scheduler_end"
+
+TEST=="%S%p/mq", ENV{.IS_MQ}="1"
+ENV{.IS_MQ}=="1", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="kyber", ENV{SCHED}="kyber"
+ENV{.IS_MQ}=="1", ATTR{queue/rotational}!="0", ATTR{queue/scheduler}="bfq", ENV{SCHED}="bfq"
+ENV{.IS_MQ}!="1", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="deadline", ENV{SCHED}="deadline"
+ENV{.IS_MQ}!="1", ATTR{queue/rotational}!="0", ATTR{queue/scheduler}="cfq", ENV{SCHED}="cfq"
+
+LABEL="ssd_scheduler_end"
+EOF
+
+nvl=0
+while [[ $nvl -lt ${#VLANS[@]} ]]; do
+	_add_vlan eth0 $nvl
+	# echo eth0.$nvl > /sys/module/rdma_rxe/parameters/add
+	: $((nvl++))
+done
+
+ip link show eth0 | grep $MAC_ADDR1
+if [ $? -eq 0 ]; then
+	BLKDEV=$(_zram_hot_add "1G")
+	[ -b "$BLKDEV" ] || \
+		_fatal "$BLKDEV device not available"
+	_nvmet_add_subsys ${SUBSYS} || \
+		_fatal
+	_nvmet_add_namespace ${SUBSYS} 1 ${BLKDEV}
+
+#	_nvmet_create_rdma_port 1 ${IP_ADDR1}
+#	_nvmet_link_subsys_to_port ${SUBSYS} 1
+
+	np=1
+	for vl in ${VLANS[@]}; do
+		_nvmet_create_rdma_port $np ${vl}${IP_ADDR1#$SUBNET}
+		_nvmet_link_subsys_to_port ${SUBSYS} $np
+		: $((np++))
+#		break
+	done
+
+#	ln -s /sys/kernel/config/nvmet/subsystems/rapnv /sys/kernel/config/nvmet/ports/1/subsystems/rapnv
+#	echo rxe0 >/sys/module/rdma_rxe/parameters/remove
+#	echo eth0 >/sys/module/rdma_rxe/parameters/add
+#	ln -s /sys/kernel/config/nvmet/subsystems/rapnv /sys/kernel/config/nvmet/ports/1/subsystems/rapnv
+	#	dmesg | less +F
+	#rxe_cfg add eth0
+	#rxe_cfg status
+	#rdma_server
+fi
+
+ip link show eth0 | grep $MAC_ADDR2
+if [ $? -eq 0 ]; then
+#	nvme connect -t rdma -a $IP_ADDR1 -s 4420 -n ${SUBSYS} # || _fatal
+#	echo eth0 >/sys/module/rdma_rxe/parameters/add
+	for vl in ${VLANS[@]}; do
+	    nvme discover -t rdma -a ${vl}${IP_ADDR1#$SUBNET} -s 4420
+	    nvme connect -t rdma -a ${vl}${IP_ADDR1#$SUBNET} -s 4420 -n ${SUBSYS} # || _fatal
+	done
+	udevadm settle
+	nvmedev=$(ls /dev/ | grep -Eo 'nvme[0-9]n[0-9]')
+	multipath -ll
+	multipathd show maps
+	multipathd show paths
+#	set +x
+#	dmesg | less +F
+#	rxe_cfg add eth0
+#	rxe_cfg status
+#	rdma_client -s ${IP_ADDR1}
+#	rdma_client -s ${VLANS[0]}${IP_ADDR1#$SUBNET}
+fi
+
+
+
